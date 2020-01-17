d794eca0f0b9c87da9f87dae11473b3415d8bac8
Ben Ruwel <benruwel21@gmail.com>
2020-01-17 19:58:01 +0300

    worked on responsiveness

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-$(document).ready(
-  function() {
+$(document).ready(function() {
     $('.scroll-down').on('click', function(e) {
       $('html, body, #hero').animate({ scrollTop: $('#scroll-to').offset().top}, 500, 'linear');
     });
-  }
-);
+  });
